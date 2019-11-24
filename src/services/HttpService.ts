e226feddc8a8529d7e93431cf64b8130db90be09
municorn <municorn@musicaloft.com>
2019-11-23 23:25:33 -0700

    preparing to test pushHistory

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
+import { HistoryListItem } from '@/models/HistoryListItem';
 
 export default class HttpService {
-    static pushHistory() { }
-
     static async test(): Promise<string> {
         const res = await axios.get('/api/test');
         console.log(res.data);
+        return res.data;
+    }
+
+    static async pushHistory(userID: number, title: string, priv: boolean, relatedTaskID: number): Promise<HistoryListItem> {
+        const body = {
+            userID,
+            title,
+            relatedTaskID,
+            "private": priv
+        };
+        const res = await axios.post('/api/history', body)
         return res.data;
     }
 }
