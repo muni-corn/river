db69dee52b0f21a1f25a4f3fb918c1495fcae81a
Harrison Thorne <harrison.thorne@gmail.com>
2019-11-23 23:24:44 -0700

    added initial store stuff

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { StoreActions } from "@/enums/StoreActions";
+import { HistoryListItem } from "@/models/HistoryListItem";
+import HttpService from '@/services/HttpService';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+        history: [] as HistoryListItem[]
+    },
+    mutations: {
+        [StoreActions.HistoryPush](state, newItem: HistoryListItem) {
+            state.history.push(newItem);
+        }
+    },
+    actions: {
+        async [StoreActions.HistoryPush]({ commit }, newItem) {
+            HttpService.pushHistory(newItem);
+            commit(StoreActions.HistoryPush, newItem);
+        }
+    },
     modules: {}
 });
