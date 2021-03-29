abe055ce9a38721000a3900f353e161eb53bb295
Harrison Thorne <harrisonthorne@protonmail.com>
2021-03-29 12:43:10 -0600

    Add console_error_panic_hook

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod components;
 mod task;
 
 use wasm_bindgen::prelude::*;
+use std::panic;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,6 +15,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // This is the entry point for the web app
 fn main() -> Result<(), JsValue> {
+    // set console panic hook
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<app::App>();
     Ok(())
