6dff7f5be60492064a66e6589be40769511ab31e
municorn <municorn@musicaloft.com>
2021-04-01 21:38:29 -0600

    (wip): Add HistoryAgent

--- src/agents/history/mod.rs
+++ src/agents/history/mod.rs
@@ -1,36 +1,46 @@
-use yew::agent::{Agent, Context};
+use yew::agent::{Agent, AgentLink, Context, HandlerId};
+
+use crate::{history_item::HistoryItem, HistoryId, UserId};
 
 struct HistoryAgent {
-
+    link: AgentLink<Self>,
+    user_id: String,
+    data: Vec<HistoryItem>,
 }
 
 enum Msg {
     SetUserId(String),
 }
 
-enum Input {
-
+enum Request {
+    GetAllHistory,
+    GetSingle(HistoryId),
+    Subscribe(UserId),
 }
 
 enum Output {
-
+    AllHistory(Vec<HistoryItem>),
+    Single(HistoryItem),
 }
 
 impl Agent for HistoryAgent {
     type Reach = Context<Self>;
     type Message = Msg;
-    type Input = Input;
+    type Input = Request;
     type Output = Output;
 
-    fn create(link: yew::worker::AgentLink<Self>) -> Self {
-        todo!()
+    fn create(link: AgentLink<Self>) -> Self {
+        Self {
+            link,
+
+        }
     }
 
     fn update(&mut self, msg: Self::Message) {
         todo!()
     }
 
-    fn handle_input(&mut self, msg: Self::Input, id: yew::worker::HandlerId) {
+    fn handle_input(&mut self, msg: Self::Input, id: HandlerId) {
         todo!()
     }
 }
