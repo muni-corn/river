e34988a6313ca207c471f0b403a0eb0db8d54f4f
municorn <municorn@musicaloft.com>
2021-04-02 21:12:05 -0600

    (wip): Implement history subscriptions

--- src/agents/history/mod.rs
+++ src/agents/history/mod.rs
@@ -1,46 +1,75 @@
+use crate::{history_item::HistoryItem, HistoryId, UserId};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fmt::{self, Display},
+};
 use yew::agent::{Agent, AgentLink, Context, HandlerId};
 
-use crate::{history_item::HistoryItem, HistoryId, UserId};
-
 struct HistoryAgent {
-    link: AgentLink<Self>,
-    user_id: String,
-    data: Vec<HistoryItem>,
-}
-
-enum Msg {
-    SetUserId(String),
+    event_subscribers: HashMap<HandlerId, Vec<UserId>>,
 }
 
 enum Request {
-    GetAllHistory,
+    /// Get all history items, optionally supplying a UserId to restrict the query to only the user
+    /// provided.
+    GetAllHistory(Option<UserId>),
+
+    /// Get and return a single HistoryItem.
     GetSingle(HistoryId),
+
+    /// Listen for history changes for the user id
     Subscribe(UserId),
 }
 
 enum Output {
-    AllHistory(Vec<HistoryItem>),
+    AllHistory(Option<UserId>, Vec<HistoryItem>),
     Single(HistoryItem),
 }
 
 impl Agent for HistoryAgent {
     type Reach = Context<Self>;
-    type Message = Msg;
+    type Message = ();
     type Input = Request;
-    type Output = Output;
+    type Output = Result<Output, HistoryAgentError>;
 
-    fn create(link: AgentLink<Self>) -> Self {
+    fn create(_link: AgentLink<Self>) -> Self {
         Self {
-            link,
-
+            event_subscribers: HashMap::new(),
         }
     }
 
-    fn update(&mut self, msg: Self::Message) {
-        todo!()
+    fn update(&mut self, msg: Self::Message) {}
+
+    fn handle_input(&mut self, input: Self::Input, id: HandlerId) {
+        match input {
+            Request::GetAllHistory(id_opt) => (),
+            Request::GetSingle(history_id) => (),
+            Request::Subscribe(user_id) => {
+                if let Some(v) = self.event_subscribers.get_mut(&id) {
+                    v.push(user_id);
+                }
+            }
+        }
     }
 
-    fn handle_input(&mut self, msg: Self::Input, id: HandlerId) {
-        todo!()
+    fn connected(&mut self, id: HandlerId) {
+        self.event_subscribers.entry(id).or_insert_with(Vec::new);
+    }
+
+    fn disconnected(&mut self, id: HandlerId) {
+        self.event_subscribers.remove(&id);
     }
 }
+
+/// An error, the string being a message
+#[derive(Debug)]
+pub struct HistoryAgentError(String);
+
+impl Display for HistoryAgentError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "history agent error: {}", self.0)
+    }
+}
+
+impl Error for HistoryAgentError {}
