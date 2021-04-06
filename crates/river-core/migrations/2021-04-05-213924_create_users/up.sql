0d38466da0fcac7286360e50bc93640fe81b7fa0
municorn <municorn@musicaloft.com>
2021-04-05 19:28:38 -0600

    core/migrations: Add `present` field

--- crates/river-core/migrations/2021-04-05-213924_create_users/up.sql
+++ crates/river-core/migrations/2021-04-05-213924_create_users/up.sql
@@ -1,7 +1,15 @@
 create table if not exists users (
 	id						serial primary key not null,
 
-	-- if both current_task and away_reason are null, the user isn't present
+	-- if `present` is true:
+	-- *	when current_away_reason isn't null, the corresponding enum is
+	--		UserStatus::Away(<current_away_reason>)
+	-- *	when current_away_reason is null but current_task is not, the
+	--		corresponding enum is UserStatus::Working(<current_task>)
+	-- *	when both current_task and current_away_reason are null, the
+	--		corresponding enum is UserStatus::Working(None)
+	-- and, of course, if `present` is false, the enum is UserStatus::Out
+	present					boolean not null,
 	current_task			integer, -- foreign key
 	current_away_reason		text,
 
