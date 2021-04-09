226781ec1cf6baa4ecc087526015c956c39f29d0
municorn <municorn@musicaloft.com>
2021-04-09 14:54:43 -0600

    (wip): HTTP requests

--- crates/river-ui/src/components/river.rs
+++ crates/river-ui/src/components/river.rs
@@ -1,13 +1,22 @@
-use river_core::UserId;
-use yew::prelude::*;
 use super::{HistoryList, StatusWindow, TodoList};
+use river_core::UserId;
+use yew::{
+    format::{Nothing, Text},
+    prelude::*,
+    services::{
+        fetch::{FetchTask, Request, Response},
+        FetchService,
+    },
+};
 
 pub struct River {
+    link: ComponentLink<Self>,
     user_id: UserId,
 }
 
 pub enum Msg {
-
+    SendHttpRequest,
+    FetchReady,
 }
 
 #[derive(Clone, Properties)]
@@ -22,11 +31,15 @@ impl Component for River {
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
-            user_id: props.user_id
+            link,
+            user_id: props.user_id,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        if matches!(msg, Msg::SendHttpRequest) {
+            self.fetch_text();
+        }
         true
     }
 
@@ -37,10 +50,23 @@ impl Component for River {
     fn view(&self) -> Html {
         html! {
             <div class="column">
+                <button class="button" onclick=self.link.callback(|_| Msg::SendHttpRequest)>{"Http"}</button>
                 <HistoryList user_id=self.user_id />
                 <StatusWindow user_id=self.user_id />
                 <TodoList user_id=self.user_id />
             </div>
         }
+    }
+}
+
+impl River {
+    fn fetch_text(&mut self) -> FetchTask {
+        let callback = self.link.callback(move |res: Response<Text>| {
+            log::info!("{:#?}", res);
+            Msg::FetchReady
+        });
+
+        let request = Request::get("/api/test/15").body(Nothing).unwrap();
+        FetchService::fetch(request, callback).unwrap()
     }
 }
